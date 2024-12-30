0cb6a284936ee3a2a91f33d61939daa95ccb869b
Farhan <farhankhalid.dev@gmail.com>
2024-12-30 20:26:21 +0500

    put in the vercel api link in the config.js

--- FullStack-Combined/frontend/src/config.js
+++ FullStack-Combined/frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     apiUrl: process.env.NODE_ENV === 'production' 
-      ? 'https://your-vercel-app-name.vercel.app' // Update this after deploying
+      ? 'https://registration-backend-gilt.vercel.app/' // Update this after deploying
       : 'http://localhost:3000'
   };
   
