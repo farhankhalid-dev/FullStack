9cb1382a1c4dff5d332bf7ca0da0c405d48c14bb
Farhan <farhankhalid.dev@gmail.com>
2024-12-30 20:49:29 +0500

    fixed it, turns out I was missing an import... ._.

--- FullStack-Combined/frontend/src/config.js
+++ FullStack-Combined/frontend/src/config.js
@@ -1,6 +1,10 @@
 const config = {
     apiUrl: process.env.NODE_ENV === 'production' 
+<<<<<<< HEAD
       ? 'https://registration-backend-demgw40r4-farhankhalid-devs-projects.vercel.app'
+=======
+      ? 'https://registration-backend-gilt.vercel.app' // Update this after deploying
+>>>>>>> 486b46b (fixed it, turns out I was missing an import... ._.)
       : 'http://localhost:3001'
   };
   
