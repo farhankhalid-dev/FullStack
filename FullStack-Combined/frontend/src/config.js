8b13c9645d50374c1b788c240efab65a9e238c39
Farhan <farhankhalid.dev@gmail.com>
2024-12-30 22:59:14 +0500

    removed the /

--- FullStack-Combined/frontend/src/config.js
+++ FullStack-Combined/frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     apiUrl: process.env.NODE_ENV === 'production' 
-      ? 'https://nodejs-serverless-function-express-nu-beryl.vercel.app/'
+      ? 'https://nodejs-serverless-function-express-nu-beryl.vercel.app'
       : 'http://localhost:3001'
   };
   
